55186002476bee0e8e4757a36d1845e97df88f5c
IamGunpoint <iamgunpoint@gmail.com>
2025-12-25 23:39:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ catch {
 const mineflayer = require('mineflayer');
 
 // ✅ CONFIG
-const IP = 'ubaid.minekeep.gg';       // Minecraft server IP
-const PORT = 25565;              // Server port
+const IP = 'friends-z9Y6.aternos.me';       // Minecraft server IP
+const PORT = 12797;              // Server port
 const WEB_PORT = process.env.PORT || 3000; // Fake port for Render/web host
 const PASSWORD = 'Mishra@123';
 
